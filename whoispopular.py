da6d76d5e03e14db4121a21a982cffa7acf5c71f
Surya Cheedella <7132995+SnShine@users.noreply.github.com>
2016-01-10 00:00:04 +0530

    added custom x axis and labels to it along with 'By @SnShines' tag

--- whoispopular.py
+++ whoispopular.py
@@ -3,6 +3,7 @@ import confidential, sg_filter
 from pytrends.pyGTrends import pyGTrends
 import tweepy
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def get_googleTrends(searchterms, period= None):
@@ -26,10 +27,23 @@ def plotData(timeseries_title, timeseries_data):
     # plt.plot(radius, square, marker='o', linestyle='--', color='r', label='Square')
 
     time_axis= [i+1 for i in range(len(timeseries_data))]
-    #print(time_axis)
 
-    # area = [3.14159, 12.56636, 28.27431, 50.26544, 78.53975, 113.09724]
-    # square = [1.0, 4.0, 9.0, 16.0, 25.0, 36.0]
+    #custom x labels
+    time_ticks_diff= int(len(timeseries_data)/5)
+    time_ticks= [1, time_ticks_diff, 2*time_ticks_diff, 3*time_ticks_diff, 4*time_ticks_diff, len(timeseries_data)-1]
+    print(time_ticks)
+    time_labels= [timeseries_data[a][0] for a in time_ticks]
+    #change latest
+    if timeseries_title[0]== "Time":
+        time_labels[-1]= "Now"
+    elif timeseries_title[0]== "Day":
+        time_labels[-1]= "Today"
+    else:
+        time_labels[-1]= "This week"
+
+    print(time_labels)
+
+    plt.xticks(time_ticks, time_labels)
 
     for i in range(len(timeseries_title)- 1):
         y_axis= [(a[i+1]) for a in timeseries_data]
@@ -42,9 +56,9 @@ def plotData(timeseries_title, timeseries_data):
 
 
 
-    # plt.xlabel('Radius/Side')
+    plt.xlabel(timeseries_title[0])
     # plt.ylabel('Area')
-    # plt.title('Area of Shapes')
+    plt.title('By @SnShines')
     plt.legend()
     #plt.draw()
 
@@ -69,14 +83,15 @@ if __name__== "__main__":
     tweet= "artificial intelligence, machine learning"
     tweet_keywords= len(tweet.split(","))
 
-    raw_data= get_googleTrends(tweet)
-    # print(raw_data)
+    raw_data= get_googleTrends(tweet, "today 2-d")
+    #raw_data= get_googleTrends(tweet)
 
-    timeseries_title, timeseries_data= parseData(raw_data)
+    if raw_data is not None:
+        timeseries_title, timeseries_data= parseData(raw_data)
 
-    # outfile= open("tmp_data.txt", "w")
-    # outfile.write(raw_data)
-    # outfile.close()
+        outfile= open("tmp_data1.txt", "w")
+        outfile.write(raw_data)
+        outfile.close()
 
-    plotData(timeseries_title, timeseries_data)
+        plotData(timeseries_title, timeseries_data)
 

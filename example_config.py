baf19f538c3cd006e922d34a9f6f9093f265a581
Surya Cheedella <7132995+SnShine@users.noreply.github.com>
2016-01-10 16:00:30 +0530

    modified structure of confing file

---
+++ example_config.py
@@ -0,0 +1,6 @@
+google= {"User_Name": "your mail", "Password": "your password"}
+
+twitter= {"API_Key": "your key",
+            "API_Secret": "your secret key",
+            "Access_Token": "4729137445-your token",
+            "Access_Token_Secret": "your secter token"}
